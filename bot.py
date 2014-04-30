00fc5d4660b30bb1d9dce47512a5a56233675450
Kevin Spevak <jimmy@jimmy-VirtualBox.(none)>
2014-04-30 15:58:07 -0700

    switched subreddit from bottest to smashbros and cleaned up some comments

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ reddit.login(os.environ['REDDIT_USERNAME'], os.environ['REDDIT_PASSWORD'])
  
 regex = r'\.tk'
 message = "I have reported this link because it looks like malware to me.  PM me if you think this was a mistake."
-sub = 'bottest'
+sub = 'smashbros'
 
 for post in praw.helpers.submission_stream(reddit, sub):
     #id = str(post.id)
@@ -46,18 +46,3 @@ for post in praw.helpers.submission_stream(reddit, sub):
       except praw.errors.RateLimitExceeded as err:
           print("Rate Limit Exceeded \n" + str(err), file=sys.stderr)
           time.sleep(err.sleep_time)     
-
-#for comment in praw.helpers.comment_stream(reddit, '+'.join(subreddits)):
-#  cid = str(comment.id)
-#  match = re.search(regex, comment.body, re.IGNORECASE)
-#  if match and not(already.get(cid)):
-#    try:
-#      comment.reply(reply)
-#      already.set(cid, "True")
-    # If you are banned from a subreddit, reddit throws a 403 instead of a helpful message :/
-#    except HTTPError as err:
-#      print("Probably banned from /r/" + str(comment.subreddit), file=sys.stderr)
-    # This one is pretty rare, since PRAW controls the rate automatically, but just in case
-#    except praw.errors.RateLimitExceeded as err:
-#      print("Rate Limit Exceeded:\n" + str(err), file=sys.stderr)
-#      time.sleep(err.sleep_time)
